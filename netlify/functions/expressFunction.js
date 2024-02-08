be340f5be33b6249a1315b38e147ba552b489bb4
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 22:43:27 +0700

    :fire:net+express

--- netlify/functions/expressFunction.js
+++ netlify/functions/expressFunction.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors'); // Import the CORS middleware
+
 const app = express();
+
+// Use the CORS middleware
+app.use(cors());
 
 app.get('/.netlify/functions/expressFunction', (req, res) => {
     res.json({ message: 'Hello from Netlify function with Express.js!' });
