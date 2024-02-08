2d5cafe06f5592795427f9c39c854854ae144f00
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 22:49:17 +0700

    :fire:net+express

--- netlify/functions/expressFunction.js
+++ netlify/functions/expressFunction.js
@@ -11,5 +11,10 @@ app.get('/.netlify/functions/expressFunction', (req, res) => {
     res.json({ message: 'Hello from Netlify function with Express.js!' });
 });
 
+app.get('/.netlify/functions/random', (req, res) => {
+    let random = Math.floor(Math.random() * 100);
+    res.json({ message: `Random number: ${random}` });
+});
+
 // Export the handler function
 module.exports.handler = serverless(app);
