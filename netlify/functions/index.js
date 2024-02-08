be31f9da0cc7f1f12d1cdf37d4f7d2675fcbdf3a
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 22:56:39 +0700

    :fire:net+express

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -5,5 +5,4 @@ const app = require('./expressFunction');
 const netlifyFunction = express();
 netlifyFunction.use('/.netlify/functions/expressFunction', app);
 
-// Export the handler function
-module.exports.handler = serverless(netlifyFunction);
+module.exports = netlifyFunction;
