7c5d9ba4bd6def3e52e7ffc647145ba7388a5b8c
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 23:00:41 +0700

    :fire:net+express

--- netlify/functions/expressFunction.js
+++ netlify/functions/expressFunction.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const cors = require('cors');
-
+const cors = require('cors'); // Import the CORS middleware
 const app = express();
-
+const baseUrl = '/.netlify/functions/expressFunction';
 // Use the CORS middleware
 app.use(cors());
 
-// Route to handle requests to /api/data
-app.get('/api/data', (req, res) => {
+app.get(baseUrl, (req, res) => {
     res.json({ message: 'Hello from Netlify function with Express.js!' });
 });
 
-// Route to generate a random 6-digit number
-app.get('/api/random-number', (req, res) => {
-    const randomNumber = Math.floor(100000 + Math.random() * 900000); // Generate random number between 100000 and 999999
-    res.json({ randomNumber });
+app.get(baseUrl+"random-number", (req, res) => {
+    let random = Math.floor(Math.random() * 100);
+    res.json({ message: `Random number: ${random}` });
 });
 
 // Export the handler function
