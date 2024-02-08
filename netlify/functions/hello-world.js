557350d0e3465d84a0b0cebdb493c1bbad9ee5af
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 22:14:06 +0700

    :fire:add func

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,22 +1,21 @@
 const handler = async (event) => {
     try {
-      const subject = event.queryStringParameters.name || 'World'
-      return {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*", // Adjust this to your frontend URL if possible
-          "Access-Control-Allow-Headers": "Content-Type, hx-request", // Include 'hx-request' in the allowed headers
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
-        },
-        body: JSON.stringify({ message: `Hello ${subject}` }),
-        // // more keys you can return:
-        // headers: { "headerName": "headerValue", ... },
-        // isBase64Encoded: true,
-      }
+        const subject = event.queryStringParameters.name || 'World'
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Adjust this to your frontend URL if possible
+                "Access- Control - Allow - Headers": "*",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+            },
+            body: JSON.stringify({ message: `Hello ${subject}` }),
+            // // more keys you can return:
+            // headers: { "headerName": "headerValue", ... },
+            // isBase64Encoded: true,
+        }
     } catch (error) {
-      return { statusCode: 500, body: error.toString() }
+        return { statusCode: 500, body: error.toString() }
     }
-  }
-  
-  module.exports = { handler }
-  
+}
+
+module.exports = { handler }
