d8ddd1609643798244bc9301295fdae214d0dff8
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-08 22:30:27 +0700

    :fire:net+express

--- netlify/functions/expressFunction.js
+++ netlify/functions/expressFunction.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+// Define a middleware function
+function myMiddleware(req, res, next) {
+    console.log('This is a middleware function');
+    next();
+}
+
+// Apply the middleware to all routes
+app.use(myMiddleware);
+
 // Define a route
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello from Netlify function!' });
 });
 
-// Export the handler function
-exports.handler = async (event, context) => {
-    return serverless(app)(event, context);
-};
+// Export the Express app
+module.exports = app;
--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -5,5 +5,4 @@ const app = require('./expressFunction');
 const netlifyFunction = express();
 netlifyFunction.use('/.netlify/functions/expressFunction', app);
 
-// Export the Netlify function
 module.exports = netlifyFunction;
