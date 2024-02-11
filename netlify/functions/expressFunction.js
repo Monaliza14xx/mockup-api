d39ae759733b0882c5230f0e6ae6947dbf5695a6
Monaliza14xx <mr.sounakhen@gmail.com>
2024-02-11 16:30:55 +0700

    :fire:add new api

--- netlify/functions/expressFunction.js
+++ netlify/functions/expressFunction.js
@@ -10,9 +10,17 @@ app.get(baseUrl, (req, res) => {
     res.json({ message: 'Hello from Netlify function with Express.js!' });
 });
 
-app.get(baseUrl+"/random-number", (req, res) => {
+app.get(baseUrl + "/random-number", (req, res) => {
     let random = Math.floor(Math.random() * 100);
     res.json({ message: `Random number: ${random}` });
+});
+app.post(baseUrl + "register", (req, res) => {
+    const data = req.body;
+    const name = data.name;
+    const email = data.email;
+    res.json({
+        message: `User ${name} with email ${email} registered`
+    });
 });
 
 // Export the handler function
